285e3c11dd9c29760561de1b2606fdf3a82a02c8
5ca9fac3 <akshayus27@gmail.com>
2024-09-18 16:25:47 +0530

    initial

---
+++ os/race_condition/readflag.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+    int fd = 0;
+    int size = 0;
+    char buf[256];
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <file>\n", argv[0]);
+        exit(1);
+    }
+
+    struct stat stat_data;
+    if (stat(argv[1], &stat_data) < 0)
+    {
+        fprintf(stderr, "Failed to stat %s: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    // printf("stat_data.st_uid = %d\n", stat_data.st_uid);
+
+    if (stat_data.st_uid == 0)
+    {
+        fprintf(stderr, "File %s owned by root\n", argv[1]);
+        exit(1);
+    }
+
+    fd = open(argv[1], O_RDONLY);
+
+    if (fd <= 0)
+    {
+        fprintf(stderr, "Couldn't open the file %s\n", argv[1]);
+        exit(1);
+    }
+
+    do
+    {
+        size = read(fd, buf, 256);
+        write(1, buf, size);
+    } while (size > 0);
+}
